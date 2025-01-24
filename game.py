8ae8212e632b752ca4c559ead425be41754ad799
AIM-09 <sozo.langat@gmail.com>
2025-01-24 13:01:02 +0300

    removing conflict in scoring feature

--- game.py
+++ game.py
@@ -4,8 +4,3 @@ print("Press S to Start the Game")
 # New scoring feature
 score = 0
 print(f"Your Score Is: {score}")
-<<<<<<< HEAD
-print("Main Branch update to Scoring Feature")
-=======
-print("Scoring Feature in Progress")
->>>>>>> add-scoring
